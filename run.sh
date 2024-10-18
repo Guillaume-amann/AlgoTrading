63a987c7cfc57f4749230b09b52ee41559a0eaa2
Raspberry <amann.guill@gmail.com>
2024-10-18 10:55:34 +0200

    First commit from Rspberry

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "------INIT------"
+
+DURATION=40
+INTERVAL=10
+#all in seconds
+
+START=$(date +%s)
+
+while [ $(($(date +%s) - START)) -lt $DURATION ]; do
+	TEMP=$(vcgencmd measure_temp)
+	echo "Current temperature: $TEMP"
+
+	./MonPremierTest.exe
+	echo -e "\nDone."
+
+	TEMP=$(vcgencmd measure_temp)
+	echo "Temperature after execute: $TEMP"
+
+	sleep $INTERVAL
+done
+
+echo "------END------"
