0642e9021a55b73b02906bab45ae1c9f2738b28c
Guillaume Amann <amann.guill@gmail.com>
2024-11-03 11:17:37 +0000

    First try of Multi-processing

--- macOS_scripts/auto.sh
+++ macOS_scripts/auto.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-cd /Users/username/path/to/your/local/clone/
+cd /Users/guillaume/Downloads/Perso/Informatique/C++/AlgoTrading/
 
 echo "------------------INIT------------------"
 echo
-output=$(./Instruments/stockAnalysis)
-osascript -e "do shell script \"echo '$output \n\n See more here: https://github.com/Guillaume-amann/AlgoTrading' | mail -s 'Stock Alert' your.address@mail.com\""
+
+# Run both scripts in the background and capture their outputs
+output1=$(./Instruments/stockAnalysis 2>&1 & pid1=$!)
+output2=$(./Instruments/optionPricer 2>&1 & pid2=$!)
+
+# Wait for both processes to finish
+wait $pid1
+wait $pid2
+
+# Send email with combined output
+osascript -e "do shell script \"echo '$output1\n\n$output2\n\nSee more here: https://github.com/Guillaume-amann/AlgoTrading' | mail -s 'Stock Alert' amann.guill@gmail.com\""
 echo "Email sent."
+echo
 echo
 echo "------------------END------------------"
