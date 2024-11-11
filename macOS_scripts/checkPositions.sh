c503a827ad798bf094a4ccac5946ca426ff458f6
Guillaume Amann <amann.guill@gmail.com>
2024-11-11 12:57:04 +0000

    changed scripts location

--- macOS_scripts/checkPositions.sh
+++ macOS_scripts/checkPositions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-cd /Users/username/path/to/your/local/clone/
+cd /Users/guillaume/AlgoTrading/
 
 cp Portfolio/Positions.csv Portfolio/copyPositions.csv
 
