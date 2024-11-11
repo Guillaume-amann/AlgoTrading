3bcf1f07df887816b28b6a1c36831d559ae6fff8
Guillaume Amann <amann.guill@gmail.com>
2024-11-11 17:26:06 +0000

    More compact code

--- Models/BlackScholesMerton.h
+++ Models/BlackScholesMerton.h
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <cmath>
-#include <complex>
 #include "../Numeric/Distributions.h"
 using namespace std;
 
 class BSMModel {
 private:
-    double S;             // Stock price
-    double K;             // Strike price
-    double T;             // Time to maturity
-    double r;             // Risk-free rate
-    double q;             // Dividend yield
-    double sigma;         // Volatility
-    char optionType;      // Option type ('C' for Call, 'P' for Put)
+    double S, K, T, r, q, sigma;
+    char optionType;
 
 public:
     BSMModel(double S, double K, double T, double r, double q, double sigma, char optionType = 'C')
@@ -21,11 +15,8 @@ public:
     double price() {
         double d1 = (log(S / K) + (r - q + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
         double d2 = d1 - sigma * sqrt(T);
-
-        if (optionType == 'C') {
-            return S * exp(-q * T) * stdN(d1) - K * exp(-r * T) * stdN(d2);
-        } else {
-            return K * exp(-r * T) * stdN(-d2) - S * exp(-q * T) * stdN(-d1);
-        }
+        return (optionType == 'C') 
+            ? S * exp(-q * T) * stdN(d1) - K * exp(-r * T) * stdN(d2)
+            : K * exp(-r * T) * stdN(-d2) - S * exp(-q * T) * stdN(-d1);
     }
 };
