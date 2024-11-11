86828057e47984646c310b4fdfa0b827b0768cec
Guillaume Amann <amann.guill@gmail.com>
2024-11-11 11:05:55 +0000

    Latest tests

--- StatTests/_build/default/IsNormal.ml
+++ StatTests/_build/default/IsNormal.ml
@@ -1,7 +1,20 @@
+(* Function to calculate the mean of a sample *)
+let mean sample =
+  let sum = List.fold_left ( +. ) 0.0 sample in
+  sum /. (float_of_int (List.length sample))
+
+(* Function to calculate the standard deviation of a sample *)
+let standard_deviation sample =
+  let m = mean sample in
+  let variance =
+    List.fold_left (fun acc x -> acc +. (x -. m) ** 2.0) 0.0 sample
+    /. (float_of_int (List.length sample - 1))
+  in
+  sqrt variance
+
+(* Function to calculate the t-statistic for a one-sample test *)
 let one_sample_t_test sample population_mean =
-  (* Some logic to calculate the t-statistic for the sample *)
+  let sample_mean = mean sample in
+  let sample_std_dev = standard_deviation sample in
   let n = float_of_int (List.length sample) in
-  let mean = List.fold_left (+.) 0. sample /. n in
-  let variance = List.fold_left (fun acc x -> acc +. (x -. mean) ** 2.) 0. sample /. n in
-  let standard_error = sqrt (variance /. n) in
-  (mean -. population_mean) /. standard_error
+  (sample_mean -. population_mean) /. (sample_std_dev /. sqrt n)
--- StatTests/_build/default/Test.ml
+++ StatTests/_build/default/Test.ml
@@ -1,7 +1,11 @@
 open IsNormal
+open Printf
 
 let () =
   let sample = [10.0; 12.0; 9.0; 11.5; 10.5] in
-  let population_mean = 10.0 in
+  let population_mean = mean sample in  (* Compute mean using the function *)
+  let population_std_dev = standard_deviation sample in  (* Compute standard deviation using the function *)
   let t_statistic = one_sample_t_test sample population_mean in
-  Printf.printf "The t-statistic for the sample is: %f\n" t_statistic
+  printf "The t-statistic for the sample is: %f\n" t_statistic;
+  printf "The population mean is: %f\n" population_mean;
+  printf "The population standard deviation is: %f\n" population_std_dev
