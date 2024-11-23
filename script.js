7072b541d3402778696521ece33468e6005d1376
Guillaume Amann <amann.guill@gmail.com>
2024-11-23 09:32:19 +0000

    got rid of webpage

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    let scrollPosition = window.scrollY;
-    let header = document.querySelector('header');
-    header.style.top = -(scrollPosition * 1000) + 'px'; // Adjust speed of scroll effect
-});
